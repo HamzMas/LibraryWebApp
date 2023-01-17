8c0c3d5072c62711a7752e215876bdd9e9b1cb19
HamzMas <32570820+HamzMas@users.noreply.github.com>
2023-01-17 19:26:48 +0000

    Add Carousel to page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./App.css";
+import { ExploreTopBooks } from "./Layouts/HomePage/ExploreTopBooks";
 import { Navbar } from "./Layouts/NavbarAndFooter/Navbar";
+import { Carousel } from "./Layouts/HomePage/Carousel";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <ExploreTopBooks />
+      <Carousel />
+    </div>
+  );
 }
 
 export default App;
